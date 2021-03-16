9d6232fb5bff3e9780e4d8951f0c261acc1d9846
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-03-16 19:36:58 +0200

    Hello gRPC

---
+++ src/hellogrpc/HelloRpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using HelloGrpc;
+using System;
+using System.Threading.Tasks;
+using static HelloGrpc.Greeter;
+
+namespace HelloRpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new GreeterClient(channel);
+
+            var request = new HelloRequest
+            {
+                Name = "Nathan"
+            };
+
+            var response = await client.SayHelloAsync(request);
+            Console.WriteLine(response.Message);
+            Console.ReadKey();
+        }
+    }
+}
