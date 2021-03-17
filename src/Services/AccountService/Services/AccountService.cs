5398c99d1e2416b62c30f8e1d5b280caf3b42d5a
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-03-17 22:44:29 +0200

    Added Account gRPC service

---
+++ src/Services/AccountService/Services/AccountService.cs
@@ -0,0 +1,15 @@
+﻿using AccountService.Protos;
+using Grpc.Core;
+using System.Threading.Tasks;
+using static AccountService.Protos.Account;
+
+namespace AccountService.Services
+{
+    public class AccountService : AccountBase
+    {
+        public override Task<AccountReply> CheckBalance(AccountRequest request, ServerCallContext context)
+        {
+            return base.CheckBalance(request, context);
+        }
+    }
+}
