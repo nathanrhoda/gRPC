24b80c6f1e4ce3addfd5ef04ecbc347eb1fbacc3
Nathan Rhoda <nathan.rhoda@hotmail.com>
2021-03-17 20:26:15 +0200

    Created bank api

---
+++ src/Services/Bank.Api/Controllers/WithdrawalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bank.Api.Controllers
+{
+    public class WithdrawalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
